b62b175165acc566c1753a5b4f13c3b8d7cd9b25
Tim Hamilton <tshamilton@users.noreply.github.com>
2020-02-08 00:01:11 +1100

    .

--- map_page.php
+++ map_page.php
@@ -31,7 +31,6 @@ function mCol ($x) {
 	}
 }
 session_start();
-
 $Team = $_SESSION['T'];
 $Tri = $_GET['t'];
 $Name = $_GET['n'];
@@ -40,22 +39,9 @@ $LAT = $_GET['lat'];
 $LNG = $_GET['lng'];
 $ZOM = $_GET['z'];
 $MAR = Array();
-/*
-if (strlen($Tri) == 3) { # If it's a national map
-	$title = "Map of ".$Name;
-	$map_init = "var map = new google.maps.Map(document.getElementById('map'), { center: new google.maps.LatLng(".$LAT.",".$LNG."), zoom: ".$ZOM." });\n";
 
-	foreach ($Team as $tm) {
-		if ($tm['Tri'] == $Tri) {
-			if (isset($tm['Loc'])) {
-				$mkr = "var marker1 = new google.maps.Marker({ position: {lat: ".$tm['Lat'].", lng: ".$tm['Long']."}, title: '".$tm['Name'].", ".$tm['Loc']."', icon: 'http://www.googlemapsmarkers.com/v1/".$tm['Pin'][0]."/".mCol($tm['Pin'][2])."/".mCol($tm['Pin'][1])."/".mCol($tm['Pin'][3])."/', map: map });\n";
-				array_push($MAR, $mkr);
-			}
-		}
-	}
-}
-else {
-	$title = "League Map";
+if ($Tri[0] == 't') {
+	$the_title = "League Map";
 	$tr = substr($Tri, 1);
 	$tot_lng = 0;
 	$tot_lat = 0;
@@ -66,12 +52,24 @@ else {
 			$tm = $tm[0];
 			if (isset($Team[$tm]['Loc'])) {
 				$tot_lat += $Team[$tm]['Lat'];
-				$tot_lng += $Team[$tm]['Lng'];
-				$mkr = "\t\t\tvar marker1 = new google.maps.Marker({ position: {lat: ".$Team[$tm]['Long'].", lng: ".$Team[$tm]['Lat']."}, title: '".$Team[$tm]['Name'].", ".$Team[$tm]['Loc']."', icon: 'http://www.googlemapsmarkers.com/v1/".$Team[$tm]['Pin'][0]."/".mCol($Team[$tm]['Pin'][2])."/".mCol($Team[$tm]['Pin'][1])."/".mCol($Team[$tm]['Pin'][3])."/', map: map });\n";
-				array_push($MAR, $start.$pos.$title.$pincol);
+				$tot_lng += $Team[$tm]['Long'];
+				$mkr = "var marker1 = new google.maps.Marker({ position: {lat: ".$Team[$tm]['Long'].", lng: ".$Team[$tm]['Lat']."}, title: '".$Team[$tm]['Name'].", ".$Team[$tm]['Loc']."', icon: 'http://www.googlemapsmarkers.com/v1/".$Team[$tm]['Pin'][0]."/".mCol($Team[$tm]['Pin'][2])."/".mCol($Team[$tm]['Pin'][1])."/".mCol($Team[$tm]['Pin'][3])."/', map: map });\n";
+				array_push($MAR, $mkr);
 			}
 		}
-		$map_init = "var map = new google.maps.Map(document.getElementById('map'), { center: new google.maps.LatLng(".($tot_lat/sizeof($MAR)).",".($tot_lng/sizeof($MAR))."), zoom: 8 });\n";
+		$LAT = $tot_lat/sizeof($MAR);
+		$LNG = $tot_lng/sizeof($MAR);
+	}
+}
+else {
+	$the_title = "Club Map of ".$Name;
+	foreach ($Team as $tm) {
+		if ($tm['Tri'] == $Tri) {
+			if (isset($tm['Loc'])) {
+				$mkr = "var marker1 = new google.maps.Marker({ position: {lat: ".$tm['Long'].", lng: ".$tm['Lat']."}, title: '".$tm['Name'].", ".$tm['Loc']."', icon: 'http://www.googlemapsmarkers.com/v1/".$tm['Pin'][0]."/".mCol($tm['Pin'][2])."/".mCol($tm['Pin'][1])."/".mCol($tm['Pin'][3])."/', map: map });\n";
+				array_push($MAR, $mkr);
+			}
+		}
 	}
 }
 ?>
@@ -80,30 +78,33 @@ else {
 	<head>
 		<style>
 			/* Always set the map height explicitly to define the size of the div
-			 * element that contains the map. 
+			 * element that contains the map. */
 			#map {
 				height: 95%;
 			}
-			/* Optional: Makes the sample page fill the window. 
+			/* Optional: Makes the sample page fill the window. */
 			html, body {
 				height: 100%;
 				margin: 0;
 				padding: 0;
 			}
 		</style>
-		<title><?php print $title; ?></title>
+		<title><?php print $the_title; ?></title>
 	</head>
 	<body>
-		<div id="map"></div>
+	<div id="map"></div>
 		<script>
 			function initMap() {
+				var map = new google.maps.Map(document.getElementById('map'), {
+					center: new google.maps.LatLng(<?php print $LAT; ?>,<?php print $LNG; ?>),
+					zoom: 8
+				});
 <?php
-	print $map_init;
 	foreach ($MAR as $m) {
 		print t(4).$m;
 	}
 ?>
-			}*/
+			}
 		</script>
 		<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC7hP3y2nf3CfHP9HSKPs9YlfDvCBOU3MY&callback=initMap"></script>
 	</body>
