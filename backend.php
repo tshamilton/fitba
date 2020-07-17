ba637fde336df3591d5298de67d8eee08ec9387e
Tim Hamilton <tshamilton@users.noreply.github.com>
2020-07-17 19:27:10 +1000

    Group name mgmt

--- backend.php
+++ backend.php
@@ -238,12 +238,14 @@ function doLadder ($c, $n) { // Country trigram, Competition Name
 		foreach ($ladder as $t) {
 			$p = explode("|", $t);
 			if ($p[0] == "group") {
-				if ($p[1] == "mls")						{	continue;	}
-				elseif ($p[1] == "uslchampionship")		{	$p[1] = "USL Championship";		array_push($table_body, t(8)."<tr><th colspan='10' class=\"darkSlate text-center py-2\">".$p[1]."</th></tr>\n");	}
-				elseif ($p[1] == "Eastern")				{	$p[1] = "Eastern Conference";	array_push($table_body, t(8)."<tr><th colspan='10' class=\"darkSlate text-center py-2\">".$p[1]."</th></tr>\n");	}
-				elseif ($p[1] == "Western")				{	$p[1] = "Western Conference";	array_push($table_body, t(8)."<tr><th colspan='10' class=\"darkSlate text-center py-2\">".$p[1]."</th></tr>\n");	}
-				elseif ($p[1] == "SupportersShield")	{	$p[1] = "Supporter's Shield";	array_push($table_body, t(8)."<tr><th colspan='10' class=\"darkSlate text-center py-2\">".$p[1]."</th></tr>\n");	}
-				else 									{	array_push($table_body, t(8)."<tr><th colspan='10' class=\"darkSlate text-center py-2\"><i><b>".$p[1]."</b></i></th></tr>\n"); }
+				if ($p[1] == "mls" || $p[1] == $n)					{	continue;						}
+				elseif (preg_match("/^group(.+?)/i", $p[1], $gp))	{	$p[1] = "Group ".$gp[0];		}
+				elseif ($p[1] == "uslchampionship")					{	$p[1] = "USL Championship";		}
+				elseif ($p[1] == "Eastern")							{	$p[1] = "Eastern Conference";	}
+				elseif ($p[1] == "Western")							{	$p[1] = "Western Conference";	}
+				elseif ($p[1] == "SupportersShield")				{	$p[1] = "Supporter's Shield";	}
+				else 												{	$p[1] = "<i>".$p[1]."</i>";		}
+				array_push($table_body, t(8)."<tr><th colspan='10' class=\"darkSlate text-center py-2\"><b>".$p[1]."</b></th></tr>\n");
 			}
 			else {
 				#   0   1 2 3 4 5 6 7   8
