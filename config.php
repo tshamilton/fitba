28b5f543d6e54642b26e60750c3b2a46b8bfeba7
Tim Hamilton <tshamilton@users.noreply.github.com>
2020-11-26 21:53:44 +1100

    config update

--- config.php
+++ config.php
@@ -234,6 +234,10 @@ foreach ($the_nats as $in_t) {
 	<head>
 		<title>Fitba Stats and Config</title>
 		<!-- Required meta tags -->
+		<meta name="apple-mobile-web-app-title" content="Fitba">
+		<meta name="application-name" content="Fitba">
+		<meta name="msapplication-TileColor" content="#2d89ef">
+		<meta name="theme-color" content="#ffffff">
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<!-- My CSS links -->
@@ -247,6 +251,7 @@ foreach ($the_nats as $in_t) {
 		<link rel="apple-touch-icon" sizes="180x180" href="image/apple-touch-icon.png">
 		<link rel="icon" type="image/png" sizes="32x32" href="image/favicon-32x32.png">
 		<link rel="icon" type="image/png" sizes="16x16" href="image/favicon-16x16.png">
+		<link rel="shortcut icon" type="image/png" href="image/favicon.ico"/>
 		<link rel="mask-icon" href="image/safari-pinned-tab.svg" color="#2d89ef">
 	</head>
 	<body>
@@ -950,8 +955,8 @@ foreach ($the_nats as $in_t) {
 			</div>
 			</div><!-- End tab panel -->
 		</div>
-		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+  		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 	</body>
 </html>
