52874bb2dd5047dbacee3a03cf40c2c8d9086872
Tim Hamilton <tshamilton@users.noreply.github.com>
2020-12-18 19:34:46 +1100

    attempt to fix #2

--- index.php
+++ index.php
@@ -5,29 +5,23 @@
 <html lang="en">
 	<head>
 		<title>Fitba News</title>
-		<link rel="shortcut icon" type="image/png" href="image/favicon.ico"/>
-		<!-- Required meta tags -->
+<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="apple-mobile-web-app-title" content="Fitba">
 		<meta name="application-name" content="Fitba">
 		<meta name="msapplication-TileColor" content="#2d89ef">
 		<meta name="theme-color" content="#ffffff">
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<!-- My CSS links -->
+		<link rel="shortcut icon" type="image/png" href="image/favicon.ico"/>
 		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Montserrat">
-		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<!-- Bootstrap CSS -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 		<link rel="stylesheet" href="css/base.css">
 		<link rel="stylesheet" href="css/style.css">
 		<link rel="stylesheet" href="css/badge.css">
-		<link rel="apple-touch-icon" sizes="180x180" href="image/apple-touch-icon.png">
-		<link rel="icon" type="image/png" sizes="32x32" href="image/favicon-32x32.png">
-		<link rel="icon" type="image/png" sizes="16x16" href="image/favicon-16x16.png">
-		<link rel="mask-icon" href="image/safari-pinned-tab.svg" color="#2d89ef">
 	</head>
 	<body>
-		<script src="https://code.jquery.com/jquery-3.5.1.slim.js" integrity="sha256-DrT5NfxfbHvMHux31Lkhxg42LY6of8TaYyK50jnxRnM=" crossorigin="anonymous"></script>
 		<ul class="nav nav-pills nav-fill justify-content-center">
 <?php
 	doTabs($tablist);
@@ -41,8 +35,6 @@
 	missing("Send");
 ?>
 		</div>
-		<script src="https://code.jquery.com/jquery-3.5.1.slim.js" integrity="sha256-DrT5NfxfbHvMHux31Lkhxg42LY6of8TaYyK50jnxRnM=" crossorigin="anonymous"></script>
-  		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-  		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 	</body>
 </html>
